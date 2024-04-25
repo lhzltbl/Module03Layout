d2aef41de39264732f9d7c65e64c0d90c4c05711
Lhizel Tabual <tabual.lhizel@auf.edu.ph>
2024-04-26 00:23:20 +0800

    intial commit

---
+++ Module03Layout/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Module03Layout
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void StackLayout_Page(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new StackLayoutPage());
+        }
+
+        private async void AbsoluteLayout_Page(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new AbsoluteLayoutPage());
+        }
+
+        private async void RelativeLayout_Page(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new RelativeLayoutPage());
+        }
+
+        private async void Grid_Page(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new GridPage());
+        }
+    }
+}
